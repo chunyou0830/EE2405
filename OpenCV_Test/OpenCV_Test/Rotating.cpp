639e623cfb810b13ca4612b5b686c38e5b05d3bf
chunyou0830 <chunyou0830@gmail.com>
2016-03-27 11:46:18 +0800

    Edge detection and line detection update

--- OpenCV_Test/OpenCV_Test/Rotating.cpp
+++ OpenCV_Test/OpenCV_Test/Rotating.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 int edgeThresh = 50;
 void main(){
-	cv::Mat srcImg = cv::imread("D:/book.jpg", CV_LOAD_IMAGE_UNCHANGED);
-	cv::blur(srcImg, srcImg, cv::Size(10, 10));
+	Mat srcImg = imread("D:/book.jpg", CV_LOAD_IMAGE_UNCHANGED);
+	blur(srcImg, srcImg, Size(10, 10));
 	IplImage* pSrcImg = &IplImage(srcImg);
 	IplImage* pDstImg = cvCreateImage(cvGetSize(pSrcImg), 8, 1);
 	IplImage* pColorImg = cvCreateImage(cvGetSize(pSrcImg), 8, 3);
@@ -22,9 +22,6 @@ void main(){
 	// Canny Edge Detection
 	cvCanny(pSrcImg, pDstImg, 100, 100, 3);
 
-	// CV_BGR2GRAY Grayscale Conversion
-	//cvCvtColor(pDstImg, pColorImg, CV_GRAY2BGR);
-
 	// Hough Lines Detection
 	lines = cvHoughLines2(pDstImg, storage, CV_HOUGH_PROBABILISTIC, 1, CV_PI / 180, 100, 100, 100);
 
@@ -32,11 +29,11 @@ void main(){
 	for(int i = 0; i < lines->total; i++) {
 		CvPoint* line = (CvPoint*)cvGetSeqElem(lines, i);
 		cvLine(pSrcImg, line[0], line[1], CV_RGB(255, 0, 0), 3, CV_AA, 0);
+		cv::Scalar c = cv::Scalar(0, 255, 0);
 		cout << i << ": (" << line[0].x << "," << line[0].y << ") (" << line[1].x << "," << line[1].y << ")" << endl;
+		//col += 30;
 	}
-
 	// Show images
 	cvShowImage("Source", pSrcImg);
-	//cvShowImage("Hough", pColorImg);
 	cvWaitKey(0);
 }
